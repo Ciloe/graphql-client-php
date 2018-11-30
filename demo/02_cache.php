bb62dc62f412725df61691bebc2cb99b7836aecd
Jimmy E <Ciloe@users.noreply.github.com>
2018-11-30 23:23:48 +0100

    Issue #2 - Add factory on cache and client (#6)

--- demo/02_cache.php
+++ demo/02_cache.php
@@ -9,23 +9,24 @@ require_once './../vendor/autoload.php';
 $token = $argv[1];
 
 $DS = DIRECTORY_SEPARATOR;
-$pool = new \Symfony\Component\Cache\Adapter\FilesystemAdapter();
 $fileCache = __DIR__ . $DS . 'Resources' . $DS . 'cache' . $DS . 'cache.php';
 $queries = __DIR__ . $DS . 'Resources' . $DS . 'graph' . $DS . 'queries';
 $fragments = __DIR__ . $DS . 'Resources' . $DS . 'graph' . $DS . 'fragments';
-$adapter = new \Symfony\Component\Cache\Adapter\PhpArrayAdapter($fileCache, $pool);
 $queryParser = new \GraphQLClientPhp\Parser\QueryBasicQueryParser();
 
-$cache = new \GraphQLClientPhp\Cache\BasicCache(
-    $adapter,
-    $queryParser,
-    ['queries' => $queries, 'fragments' => $fragments]
+$cache = \GraphQLClientPhp\Cache\BasicCache::factory(
+    $fileCache,
+    $queries,
+    $fragments,
+    $queryParser
 );
-$cache->warmUp();
 
-$model = new \GraphQLClientPhp\Model\ApiModel('https://api.github.com', 'graphql', $token);
-$bridge = new \GraphQLClientPhp\Bridge\BasicBridge($model);
-$client = new \GraphQLClientPhp\Client\BasicClient($bridge, $queryParser);
+$client = \GraphQLClientPhp\Client\BasicClient::factory(
+    'https://api.github.com',
+    'graphql',
+    $token,
+    $queryParser
+);
 
 $results = $client
     ->setVariables(['number' => 5])
